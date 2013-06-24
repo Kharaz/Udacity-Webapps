2b50d8cfff63f65d953d077627799557815d6ca3
Simon Rupp-Greene <muffintractor@gmail.com>
2013-06-24 10:25:47 -0600

    main.py update

--- simonruppgreene-webapps/main.py
+++ simonruppgreene-webapps/main.py
@@ -16,10 +16,78 @@
 #
 import webapp2
 
+form="""
+
+<form method="post">
+    What is your Birthday?
+    <br>
+    <label>
+        Month
+        <input type="text" name = "month" value="%(month)s">
+    </label>
+    <label>
+        Day
+        <input type="text" name="day" value="%(day)s">
+    </label>
+    <label>
+        Year
+        <input type="text" name="year" value"%(year)s">
+    </label>
+    <div style="color: red">%(error)s</div>
+    <br><br>
+    <input type="submit">
+</form>
+
+
+"""
+
+
+
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error":error,
+                                        "month":month,
+                                        "day":day,
+                                        "year":year
+                                        })
+    
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+    def post(self):
+        user_month = valid_month(self.request.get('month'))
+        user_day = valid_day(self.request.get('day'))
+        user_year = valid_year(self.request.get('year'))
+
+        if not(user_month and user_day and user_year):
+            self.write_form("That doesn't look valid to me, friend.")
+        else:
+            self.response.out.write("Thanks! Valid!")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+months= ["Jaunary",
+         "February",
+         "March",
+         "April",
+         "May",
+         "June",
+         "July",
+         "August",
+         "September",
+         "October",
+         "November",
+         "December"]
+
+
+def valid_month(month):
+    
+
+def valid_day(day):
+    
+
+def valid_year(year):
+    
+    
