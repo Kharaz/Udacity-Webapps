a5fffcdace3b51c6181eed9e36cce1ff5904732a
Simon Rupp-Greene <muffintractor@gmail.com>
2013-08-12 16:06:38 -0600

    Password hashing
    Basic hashing functionality, not implemented

---
+++ simonruppgreene-webapps/handlers/passHash.py
@@ -0,0 +1,19 @@
+# This module contains functions for making a secure
+# and salty password verification/hash generation system.
+
+import random
+import string
+import hashlib
+
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (h, salt)
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name, pw, salt)
