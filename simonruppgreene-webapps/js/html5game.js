0a17345b2a60e8bc711fce297a5756afd6e6e01b
Simon Rupp-Greene <muffintractor@gmail.com>
2013-11-21 13:25:10 -0700

    fixing things please?

--- simonruppgreene-webapps/js/html5game.js
+++ simonruppgreene-webapps/js/html5game.js
@@ -1,33 +1,6 @@
-
-/* currently working on this part
-var world = {
-	CANVAS_WIDTH: 0,
-	CANVAS_HEIGHT: 0,
-	fps: 30,
-	worldCanvas: null,
-	objects: []
-}
-
-world.instantiate = function(width, height){
-	world.CANVAS_WIDTH = width;
-	world.CANVAS_HEIGHT = height;
-
-	var canvasElement = $("<canvas width='" + world.CANVAS_WIDTH +
-					      "' height='" + world.CANVAS_HEIGHT + "'></canvas>");
-
-	worldCanvas = canvasElement.get(0).getContext("2d");
-	canvasElement.appendTo('body');
-}
-
-world.step = function(){
-	world.update();
-	world.draw();
-}
-
-world.update = function(){
-
-}
-*/
+function clear() {
+			canvas.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+		}
 
 
 
@@ -36,17 +9,11 @@ window.onload = function() {
 	var CANVAS_HEIGHT = 320;
 
 	var canvasElement = $("<canvas width='" + CANVAS_WIDTH +
-					      "' height='" + CANVAS_HEIGHT + "'></canvas>");
+						  "' height='" + CANVAS_HEIGHT + "'></canvas>");
 	var canvas = canvasElement.get(0).getContext("2d");
 	canvasElement.appendTo('body');
 
 	var FPS = 30;
-
-	var vX = 2;
-	var vY = 2;
-
-	var textX = 50;
-	var textY = 50;
 
 	setInterval(function() {
 		update();
@@ -54,25 +21,11 @@ window.onload = function() {
 	}, 1000/FPS);
 
 	function update() {
-		if(textX > 460 || textX < 0){
-			vX *= -1;
-		}
-		if(textY > 310 || textY < 10){
-			vY *= -1;
-		}
-
-		textX+=vX;
-		textY+=vY;
-	}
-
-	function clear() {
-		canvas.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+		clear();
 	}
 
 	function draw() {
-		clear()
 		canvas.fillStyle = "#000";
-		canvas.fillText("Sup.", textX, textY);
+		canvas.fillText("Sup.", 50,50);
 	}
-
 }
